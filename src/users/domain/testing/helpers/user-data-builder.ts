1afb3d91660c946326658d3011a587c50a3807ad
Murilo Martins <murilolmartins1@gmail.com>
2023-08-09 20:19:10 -0300

    test: create user data builder

---
+++ src/users/domain/testing/helpers/user-data-builder.ts
@@ -0,0 +1,21 @@
+import { UserProps } from '../../entities/user.entity'
+import { faker } from '@faker-js/faker'
+
+export const UserDataBuilder = (data?: Partial<UserProps>): UserProps => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    createdAd: faker.date.past(),
+    ...data,
+})
+
+export const UsersDataBuilder = ({
+    data,
+    quantity = 5,
+}: {
+    data?: Partial<UserProps>[]
+    quantity?: number
+}): UserProps[] =>
+    data
+        ? data.map(user => UserDataBuilder(user))
+        : Array.from({ length: quantity }, () => UserDataBuilder())
